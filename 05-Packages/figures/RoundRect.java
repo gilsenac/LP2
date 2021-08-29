a6322a5d13dc4a6ca05dfefeb66c5245ed55c60d
gilsenac <58448408+gilsenac@users.noreply.github.com>
2021-08-29 12:29:50 -0300

    Add files via upload

---
+++ 05-Packages/figures/RoundRect.java
@@ -0,0 +1,28 @@
+package figures;
+
+import java.awt.*;
+
+public class RoundRect {
+    int x, y;
+    int w, h;
+    int arcw,arch;
+
+    public RoundRect (int x, int y, int w, int h, int arcw, int arch) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.arcw = arcw;
+        this.arch = arch;
+    }
+
+    public void print () {
+        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y, this.arcw, this.arch );
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawRoundRect(this.x,this.y, this.w,this.h, this.arcw, this.arch);
+    }
+}
