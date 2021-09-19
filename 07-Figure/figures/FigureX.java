8f38334f6448cc3f386e41eea5571271ae180506
gilsenac <58448408+gilsenac@users.noreply.github.com>
2021-09-19 13:29:48 -0300

    Add files via upload

---
+++ 07-Figure/figures/FigureX.java
@@ -0,0 +1,9 @@
+package figures;
+
+import java.awt.Graphics;
+
+public abstract class FigureX {
+    int x, y;
+    int w, h;
+    public abstract void paint (Graphics g);
+}
---
+++ 07-Figure/figures/LineX.java
@@ -0,0 +1,26 @@
+package figures;
+
+import java.awt.*;
+
+public class LineX extends FigureX {
+    int z, k;
+
+    public LineX (int x, int y, int z, int k) {
+        this.x = x;
+        this.y = y; 
+        this.z = z;
+        this.k = k;
+    }
+
+    public void print () {
+        System.out.format("Linha de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.z, this.k, this.x, this.y);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawLine(this.x,this.y, this.z,this.k);
+    }
+}  
+
+
