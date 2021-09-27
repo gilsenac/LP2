180afff711e0f1ab1dc21f4f76469bb119ea1fff
gilsenac <58448408+gilsenac@users.noreply.github.com>
2021-09-26 23:07:35 -0300

    Add files via upload

---
+++ MakingofProject/MKofProjectApp.java
@@ -0,0 +1,73 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+import ivisible.IVisible;
+import figures.*;
+
+class MKofProjectApp {
+    public static void main (String[] args) {
+        IfaceFrame frame = new IfaceFrame();
+        frame.setVisible(true);
+    }
+}
+
+class IfaceFrame extends JFrame {
+    ArrayList<Figure> figs = new ArrayList<Figure>();
+    Random rand = new Random();
+
+    IfaceFrame () {
+        this.addWindowListener (
+            new WindowAdapter() {
+                public void windowClosing (WindowEvent e) {
+                    System.exit(0);
+                }
+            }
+        );
+
+        this.addKeyListener (
+            new KeyAdapter() {
+                public void keyPressed (KeyEvent evt) {
+                    int x = rand.nextInt(350);
+                    int y = rand.nextInt(350);
+                    int w = rand.nextInt(50);
+                    int h = rand.nextInt(50);
+                    if (evt.getKeyChar() == 'r') {
+                        Rect r = new Rect(x,y, w,h);
+                        figs.add(r);
+                    } else if (evt.getKeyChar() == 'e') {
+                        figs.add(new Ellipse(x,y, w,h));
+                    }
+                    repaint();
+                }
+            }
+        );
+
+        this.addMouseListener (
+            new MouseAdapter() {
+                public void mousePressed (MouseEvent evt) {
+                    int x = evt.getX();
+                    int y = evt.getY();
+                    for (Figure fig: figs) {
+                           /* System.out.println("CLICKED");*/
+                        if ((x>=fig.x)&&(x<=fig.x+fig.w)&&(y>=fig.y)&&(y<=fig.y+fig.h)) {
+                            System.out.println("CLICKED");
+                        }
+                    }
+                }
+            }
+        );
+
+        this.setTitle("Iterface IVisible");
+        this.setSize(350, 350);
+    }
+
+    public void paint (Graphics g) {
+        super.paint(g);
+        for (Figure fig: this.figs) {
+            fig.paint(g);
+        }
+    }
+}
