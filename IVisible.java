674e658c2ec00aa7e8724353afbfb6bc3fb0f127
gilsenac <58448408+gilsenac@users.noreply.github.com>
2021-10-11 23:45:20 -0300

    Delete IVisible.java

--- IVisible.java
+++
@@ -1,8 +0,0 @@
-package ivisible;
-
-import java.awt.Graphics;
-
-public interface IVisible {
-    public abstract boolean clicked (int x, int y);
-    public abstract void    paint   (Graphics g,boolean focused);
-}
