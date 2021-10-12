4033ebb2e47cdad7ed7bff5ece77816f42ff9a00
gilsenac <58448408+gilsenac@users.noreply.github.com>
2021-10-11 23:44:04 -0300

    Add files via upload

---
+++ MofProject1/figures/Rect.java
@@ -0,0 +1,32 @@
+package figures;
+
+import java.awt.*;
+
+public class Rect extends Figure {
+    public Rect (int x, int y, int w, int h) {
+        //super(x,y, w,h);
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h; 
+    }
+
+    public void print () {
+        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    public void paint (Graphics g, boolean focused) {
+        Graphics2D g2d = (Graphics2D) g;
+
+        if (focused) {
+            g2d.setPaint(Color.RED);
+            g2d.fillRect(this.x-2,this.y-2, this.w+4,this.h+4);
+        }
+
+       // g2d.setPaint(Color.WHITE);
+      //  g2d.fillRect(this.x,this.y, this.w,this.h);
+        g2d.setPaint(Color.BLACK);
+        g2d.drawRect(this.x,this.y, this.w,this.h);
+    }
+}
