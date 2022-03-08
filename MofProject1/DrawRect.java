011019ed2f153837470a8d03d20bd651ed6d815b
gilsenac <58448408+gilsenac@users.noreply.github.com>
2022-03-08 10:16:48 -0300

    Add files via upload

---
+++ MofProject1/DrawRect.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class DrawRect extends JPanel {
+    int x, y, w, h;
+
+
+    public static void main ( String[] args ) {
+        JFrame f = new JFrame ( "Draw Box Mouse 2" );
+        f.setDefaultCloseOperation ( JFrame.EXIT_ON_CLOSE );
+        f.setContentPane ( new DrawRect() );
+        f.setSize ( 300, 300 ); f.setVisible (true); 
+    }
+
+ 
+    DrawRect () {
+        int x = y = w = h;
+        MyMouseListener listener = new MyMouseListener();
+        addMouseListener ( listener );
+        addMouseMotionListener ( listener );
+    }
+
+
+
+    public void setStartPoint ( int x, int y ) {
+        this.x = x; this.y =y;
+    }
+
+    public void setEndPoint ( int x, int y ) {
+        w = Math.abs ( this.x - x ); 
+        h = Math.abs ( this.y - y );
+    }
+
+
+    class MyMouseListener extends MouseAdapter {
+        public void  mousePressed ( MouseEvent e ) {
+            setStartPoint ( e.getX(), e.getY() );
+        }
+        public void  mouseDragged ( MouseEvent e ) {
+            setEndPoint ( e.getX(), e.getY() ); repaint();
+        }
+        public void  mouseReleased ( MouseEvent e ) {
+            setEndPoint ( e.getX(), e.getY() ); repaint();
+        }
+    }
+
+
+    public void paint ( Graphics g ) {
+        super.paintComponent (g);
+        g.setColor (Color.RED);
+        g.drawRect ( x, y, w, h);
+    }
+        
+}
