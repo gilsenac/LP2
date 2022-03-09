115e5d9ecee404919f566a57c0877fefb0fea57f
gilsenac <58448408+gilsenac@users.noreply.github.com>
2022-03-08 22:15:29 -0300

    Update DrawRect.java

--- MofProject1/DrawRect.java
+++ MofProject1/DrawRect.java
@@ -3,27 +3,22 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class DrawRect extends JPanel {
-    
     int x, y, w, h;
-    
     public static void main ( String[] args ) {
         JFrame f = new JFrame ( "Draw Box Mouse 2" );
         f.setDefaultCloseOperation ( JFrame.EXIT_ON_CLOSE );
         f.setContentPane ( new DrawRect() );
         f.setSize ( 300, 300 ); f.setVisible (true); 
     }
-    
     DrawRect () {
         int x = y = w = h;
         MyMouseListener listener = new MyMouseListener();
         addMouseListener ( listener );
         addMouseMotionListener ( listener );
     }
-
     public void setStartPoint ( int x, int y ) {
         this.x = x; this.y =y;
     }
-
     public void setEndPoint ( int x, int y ) {
         w = Math.abs ( this.x - x ); 
         h = Math.abs ( this.y - y );
